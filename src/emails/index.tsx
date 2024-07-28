6c9e359fc78a02604e46007f1f6696c16526ca15
muneebhashone <muneeb.hashone@gmail.com>
2024-07-28 21:34:20 +0500

    added support for react-emails

---
+++ src/emails/index.tsx
@@ -0,0 +1,19 @@
+import { Button, Html, render } from "@react-email/components";
+import * as React from "react";
+
+export const Email = () => {
+  return (
+    <Html>
+      <Button
+        href="https://example.com"
+        style={{ background: "#000", color: "#fff", padding: "12px 20px" }}
+      >
+        Click me
+      </Button>
+    </Html>
+  );
+};
+
+export default Email;
+
+export const emailHtmlString = render(<Email />);
